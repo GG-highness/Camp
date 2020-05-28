5b91dda7aaf95562ad8817ab769e60f54dc19c44
Ubuntu <ubuntu@ip-172-31-32-218.ap-northeast-1.compute.internal>
2020-05-28 09:38:27 +0000

    アカウント作成時にlogin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(name: params[:name], 
                      email: params[:email],
-                     image_name: "default_user.jpg"
+                     image_name: "default_user.jpg",
+                     password: params[:password]
                      )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"            
       redirect_to("/users/#{@user.id}")            
     else            
