672a311173ab6108bcbc815773ffa5683b3e0a35
Ubuntu <ubuntu@ip-172-31-32-218.ap-northeast-1.compute.internal>
2020-05-28 05:58:21 +0000

    カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_023701) do
+ActiveRecord::Schema.define(version: 2020_05_28_055708) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_023701) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
