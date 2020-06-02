bd16d9b02187ab062003b28f78352d9d0f5f6918
Ubuntu <ubuntu@ip-172-31-32-218.ap-northeast-1.compute.internal>
2020-06-02 02:09:09 +0000

    バリデーションを追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,7 @@
 class Photo < ApplicationRecord
+  validates :post_id, {presence: true}
+  validates :post_image, {presence: true}
+  
   belongs_to :post, optional: true
   mount_uploader :post_image, ImageUploader
 end
