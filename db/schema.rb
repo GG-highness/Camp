60312f97374996638c3073cc678fc71228302d7e
Ubuntu <ubuntu@ip-172-31-32-218.ap-northeast-1.compute.internal>
2020-06-02 01:35:29 +0000

    Photoモデルとテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_234300) do
+ActiveRecord::Schema.define(version: 2020_06_02_013015) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer "post_id"
+    t.string "post_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
