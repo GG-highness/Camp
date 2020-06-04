a7479a07806e82db25309fc184335a4862cf2874
Ubuntu <ubuntu@ip-172-31-32-218.ap-northeast-1.compute.internal>
2020-06-04 04:27:10 +0000

    js修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,11 +18,9 @@
 
 $(function () {
   setTimeout("$('.flash').fadeOut('slow')", 900);
-});
-
-$(function(){
+  
   var $footer = $('#footer');
   if(window.innerHeight > $footer.offset().top + $footer.outerHeight() ) {
       $footer.attr({'style': 'position:fixed; top:' + (window.innerHeight - $footer.outerHeight()) + 'px;' });
   }
-})
+});
