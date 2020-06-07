b2c3957174dd53346a7a671028bd016e7100fed0
Ubuntu <ubuntu@ip-172-31-32-218.ap-northeast-1.compute.internal>
2020-06-07 13:58:14 +0000

    ensure_correct_userを再記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,6 +56,14 @@ class PostsController < ApplicationController
     redirect_to("/")
   end
 
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+  
   def post_params
     params.require(:post).permit(:content, photos_attributes: [:post_image]).merge(user_id: @current_user.id)
   end
