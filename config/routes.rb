e9ba37955723291b277559830a0447dbe4abd69b
Ubuntu <ubuntu@ip-172-31-32-218.ap-northeast-1.compute.internal>
2020-06-11 06:40:58 +0000

    root変更開始

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,6 @@ Rails.application.routes.draw do
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
   
-  get "signup" => "users#new"
-  post "users/create" => "users#create"
-  get "users/:id" => "users#show"
-  get "users/:id/edit" => "users#edit"
-  post "users/:id/update" => "users#update"
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
@@ -22,5 +17,6 @@ Rails.application.routes.draw do
   post "likes/:post_id/create" => "likes#create"
   post "likes/:post_id/destroy" => "likes#destroy"
   
+  resources :users
   resources :posts,          only: [:create,:edit, :destroy]
 end
