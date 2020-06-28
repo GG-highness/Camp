0173b5cd61132116c38f2f7513db54a3f68991c2
Ubuntu <ubuntu@ip-172-31-32-218.ap-northeast-1.compute.internal>
2020-06-28 01:53:57 +0000

    無限スクロール機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
   
   def index
-    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(4)
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(8)
   end
   
   def show
