2066e00a0dce3916b40a93d45007a3bf28e9dcb7
Dice <hosbmuhika@gmail.com>
2020-08-11 10:27:12 +0900

    userコントローラーのテストエリア

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {"test"}
+    password              {"test01"}
+    password_confirmation {"test01"}
+    sequence(:email) {Faker::Internet.email}
   end
 end
