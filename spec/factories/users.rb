9abcb1f5a1a7663c5f67517fbf86818398e262ab
Dice <hosbmuhika@gmail.com>
2020-08-24 18:09:25 +0900

    user.rb編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"test"}
+    name              {"test"}
     password              {"test01"}
     password_confirmation {"test01"}
     sequence(:email) {Faker::Internet.email}
