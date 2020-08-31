0060d6315c7a029ee824def1c4b947ceb8c548c2
Dice <hosbmuhika@gmail.com>
2020-08-31 15:28:17 +0900

    微細な修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ server '18.181.47.69',
   user: 'ec2-user', 
   roles: %w{app db web},
   ssh_options: {
-    keys: ['~/.ssh/Camp.pem'], 
+    keys: %w(~/.ssh/Camp.pem),
     forward_agent: true,
     auth_methods: %w(publickey),
     port: 22
