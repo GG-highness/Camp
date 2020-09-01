b0cd5b6e5c4c575e180e1818481b79a9f945e9c7
Dice <hosbmuhika@gmail.com>
2020-09-01 09:01:06 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 server '18.181.47.69', 
-  user: 'Dice', 
+  user: 'ec2-user', 
   roles: %w{app db web},
   ssh_options: {
-    keys: ["#{ENV('PRODUCTION_SSH_KEY')}"],  
+    keys: ['~/.ssh/Camp.pem'],  
     forward_agent: true,
     auth_methods: %w(publickey),
     port: 22
