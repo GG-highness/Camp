136e0bedf8dd041968efd906d24d2adf174bfa03
Dice <hosbmuhika@gmail.com>
2020-09-03 13:28:22 +0900

    環境変数で鍵へのパスを指定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ server '54.150.72.130',
   user: 'Dice', 
   roles: %w{app db web},
   ssh_options: {
-    keys: ['~/.ssh/Camp_key_rsa'],  
+    keys: ["#{ENV.fetch('PRODUCTION_SSH_KEY')}"]  
     forward_agent: true,
     auth_methods: %w(publickey),
     port: 22
